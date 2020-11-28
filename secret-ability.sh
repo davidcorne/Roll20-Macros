fde3315e4d6d310c784483372a4dd89fbbfb8464
davidcorne <davidcorne@gmail.com>
2020-11-28 08:36:09 +0000

    Fix the secret skill check macro generator

--- secret-ability.sh
+++ secret-ability.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/bash
 # ?{Which Skill|ACROBATICS, Alex: [[d20 + @{Alex's Character|ACROBATICS}]] Anne: [[d20 + @{Anne's Character|ACROBATICS}]]}
 
+characters=("Chad Blaze" "Sareth Kashin" "Maximus" "Minimus" "Hugo Augustus Rosebud Quentin Thethird" "Ordlin Autumfall")
+skills=("PERCEPTION" "STEALTH")
+end_braces="&#125;&#125;"
 
-# /w gm &{template:default} {{name=Group Secret Check}} {{Alex: [[d20 + @{Alex's Character|SKILL}]]}} {{Anne: [[d20 + @{Anne's Character|SKILL}]]}} {{Farran: [[d20 + @{Farran's Character|SKILL}]]}} {{Hannah: [[d20 + @{Hannah's Character|SKILL}]]}} {{James: [[d20 + @{James's Character|SKILL}]]}} {{Mark: [[d20 + @{Mark's Character|SKILL}]]}}
-characters=("Alex's Character" "Farrah's Character") # "Marla Thuzir" "Hassleback" "Lucolsheh" "Rip's Character")
-abilities="STRENGH"
-
-echo -n "?{Which Ability|"
-for ability in $abilities
+echo -n "?{Which Skill"
+for skill in ${skills[@]}
 do
-    echo "$ability,"
-    echo " /w gm &{template:default&#125; {{name=Group Secret $ability Check&#125;&#125; "
+    echo -n "|$skill,"
+    echo -n " /w gm &{template:default&#125; {{name=Group Secret $skill Check&#125;&#125; "
     for c in "${characters[@]}"
     do
-        echo -n "{{$c= [[1d20 + @{$c|${ability}_modifier}]]&#125;&#125; "
+        echo -n "{{$c=[[1d20 + @{$c|${skill}}]]&#125;&#125; "
     done
-    echo  "| "
-    echo ""
+    echo -n ""
 done
 echo "}"
 
